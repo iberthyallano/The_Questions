62f7181f03d5b02ca46b6bebc5745ed7387291c1
iberthy <iberthya@hotmail.com>
2022-10-02 20:12:42 -0300

    Criando teste do impar ou par matematico

--- __test__/ImparOuParMatematico.test.js
+++ __test__/ImparOuParMatematico.test.js
@@ -1,13 +1,13 @@
 
 test("Teste Exemplo", () => {
-    expect(diagonalPrincipalDaMatriz(3)).toBe(`Impar`);
-    expect(diagonalPrincipalDaMatriz(4)).toBe(`Par`);
+    expect(imparOuParMatematico(3)).toBe(`Impar`);
+    expect(imparOuParMatematico(4)).toBe(`Par`);
 });
 
 test("Teste Aleatorio 1", () => {
-    expect(diagonalPrincipalDaMatriz(25)).toBe(`Impar`);
+    expect(imparOuParMatematico(25)).toBe(`Impar`);
 });
 
 test("Teste Aleatorio 2", () => {
-    expect(diagonalPrincipalDaMatriz(98)).toBe(`Par`);
+    expect(imparOuParMatematico(98)).toBe(`Par`);
 });
