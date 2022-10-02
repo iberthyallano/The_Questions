c85b4eed95554559d03d8790e96d113ffffb5241
iberthy <iberthya@hotmail.com>
2022-10-02 16:38:58 -0300

    Criando teste do codigo de cesar

---
+++ __test__/CodigoDeCesar.test.js
@@ -0,0 +1,19 @@
+
+
+test('Teste Exemplo', () => {    
+    expect(
+        codigoDeCesar(3, "A ligeira raposa marrom saltou sobre o cachorro cansado")
+        ).toEqual("D oljhlud udsrvd pduurp vdowrx vreuh r fdfkruur fdqvdgr");
+});
+
+test('teste aleatorio 1', () => {
+    expect(
+        codigoDeCesar(4, "O sol e a lua")
+        ).toEqual("S vsp i e pze");
+});
+
+test('teste aleatorio 2', () => {
+    expect(
+        codigoDeCesar(2, "Papai Noel entrega presentes")
+        ).toEqual("Rcrck pqgn gpvtgic rtgugpvgu");
+});
