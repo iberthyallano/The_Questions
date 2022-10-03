cf00487f970efbaf09dd2633311ec0fc8f614c9a
iberthy <iberthya@hotmail.com>
2022-10-02 21:22:13 -0300

    Implementando a escada de asterisco

---
+++ __test__/EscadaDeAsterisco.test.js
@@ -0,0 +1,11 @@
+test('Teste Exemplo', () => {
+    expect(escadaDeAsterisco(6)).toEqual("     *\n    **\n   ***\n  ****\n *****\n******\n");
+});
+
+test('Teste Aleatorio 1', () => {
+    expect(escadaDeAsterisco(10)).toEqual("         *\n        **\n       ***\n      ****\n     *****\n    ******\n   *******\n  ********\n *********\n**********\n");
+});
+
+test('Teste Aleatorio 2', () => {
+    expect(escadaDeAsterisco(3)).toEqual("  *\n **\n***\n");
+});
