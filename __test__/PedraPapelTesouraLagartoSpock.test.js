da24f4cee6752bc0d58669f7f23a8bee3baf23c1
iberthy <iberthya@hotmail.com>
2022-10-02 23:07:11 -0300

    Criando teste do pedra papel tesoura lagarto spock

---
+++ __test__/PedraPapelTesouraLagartoSpock.test.js
@@ -0,0 +1,31 @@
+
+
+test("Teste Exemplo", () => {
+    let game = `3\npapel pedra\nlagarto tesoura\nSpock Spock`;
+    expect(Questao06(game)).toBe(`Caso #1: Bazinga!\nCaso #2: Raj trapaceou!\nCaso #3: De novo!`);
+});
+
+test("Teste Pedra", () => {
+    let game = `5\npedra pedra\npapel pedra\ntesoura pedra\nlagarto pedra\nSpock pedra\n`;
+    expect(Questao06(game)).toBe(`Caso #1: De novo!\nCaso #2: Bazinga!\nCaso #3: Raj trapaceou!\nCaso #4: Raj trapaceou!\nCaso #5: Bazinga!`);
+});
+
+test("Teste Papel", () => {
+    let game = `5\npedra papel\npapel papel\ntesoura papel\nlagarto papel\nSpock papel\n;`;
+    expect(Questao06(game)).toBe(`Caso #1: Raj trapaceou!\nCaso #2: De novo!\nCaso #3: Bazinga!\nCaso #4: Bazinga!\nCaso #5: Raj trapaceou!`);
+});
+
+test("Teste Tesoura", () => {
+    let game = `5\npedra tesoura\npapel tesoura\ntesoura tesoura\nlagarto tesoura\nSpock tesoura\n`;
+    expect(Questao06(game)).toBe(`Caso #1: Bazinga!\nCaso #2: Raj trapaceou!\nCaso #3: De novo!\nCaso #4: Raj trapaceou!\nCaso #5: Bazinga!`);
+});
+
+test("Teste Lagarto", () => {
+    let game = `5\npedra lagarto\npapel lagarto\ntesoura lagarto\nlagarto lagarto\nSpock lagarto\n`;
+    expect(Questao06(game)).toBe(`Caso #1: Bazinga!\nCaso #2: Raj trapaceou!\nCaso #3: Bazinga!\nCaso #4: De novo!\nCaso #5: Raj trapaceou!`);
+});
+
+test("Teste Spock", () => {
+    let game = `5\npedra Spock\npapel Spock\ntesoura Spock\nlagarto Spock\nSpock Spock\n`;
+    expect(Questao06(game)).toBe(`Caso #1: Raj trapaceou!\nCaso #2: Bazinga!\nCaso #3: Raj trapaceou!\nCaso #4: Bazinga!\nCaso #5: De novo!`);
+});
