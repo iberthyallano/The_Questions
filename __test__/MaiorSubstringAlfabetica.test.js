4cd9cb1ea29351f8f129446d1287011fc5c34e24
ManoelVOliS <110946352+ManoelVOliS@users.noreply.github.com>
2022-11-01 23:33:31 -0300

    Teste unitário para MaiorSubstringAlfabetica

---
+++ __test__/MaiorSubstringAlfabetica.test.js
@@ -0,0 +1,19 @@
+
+
+test('Teste Exemplo', () => {    
+    expect(
+        maiorSubstringAlfabetica("banana")
+        ).toEqual("nana");
+});
+
+test('Teste Aleatorio 1', () => {
+    expect(
+        maiorSubstringAlfabetica("abacate")
+        ).toEqual("te");
+});
+
+test('Teste Aleatorio 2', () => {
+    expect(
+        maiorSubstringAlfabetica("morango")
+        ).toEqual("rango");
+});
